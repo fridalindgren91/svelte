0e3cefcd23891eb93c5e9a5be476254b1f1c3e93
Frida Lindgren <fridalindgren_@outlook.com>
2024-03-01 12:59:12 +0100

    Small stylefix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,31 @@
-<header class="header">header</header>
-
-<slot/>
-
-<footer class="footer">footer</footer>
+<header class="header"><a href="/about">Om mig</a></header>
+<div id="slot-container">
+    <slot/>
+</div>
+<div class="container"><footer class="footer">footer</footer></div>
 
 <style>
 
+#slot-container :global() {
+    margin: 20px;
+}
+
 .header {
     background-color: grey;
+    display: flex;
+    flex-direction: row-reverse;
+    padding: 20px;
 }
+
+.container {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+
 .footer {
     background-color: grey;
+    width: 100%;
 }
 
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,2 @@
 <h1>Frida Lindgren</h1>
 <p>Portfolio</p>
-<a href="/about">Om mig</a>
