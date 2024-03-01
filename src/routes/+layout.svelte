fee8a1d534d48a3eb0b7d06c609a02ee9a9fe0b4
Frida Lindgren <fridalindgren_@outlook.com>
2024-03-01 12:15:35 +0100

    About page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<header class="header">header</header>
+
+<slot/>
+
+<footer class="footer">footer</footer>
+
+<style>
+
+.header {
+    background-color: grey;
+}
+.footer {
+    background-color: grey;
+}
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>Frida Lindgren</h1>
 <p>Portfolio</p>
+<a href="/about">Om mig</a>
